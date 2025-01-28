57d18d7ffb110e6fe9f9fc0ba107d8cd0843b89f
edwarddudas1 <edwarddudas@gmail.com>
2025-01-28 21:31:34 +0200

    adding bodyParser

--- server.mjs
+++ server.mjs
@@ -1,12 +1,16 @@
 import express from "express";
 import fs from "fs";
 import path from "path";
+import bodyParser from "body-parser";
 import { fileURLToPath } from "url";
 const app = express();
 const port = 3000;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, "public")))
 
 app.get("/", (req, res) => {
   console.log("jfjdjfk");
