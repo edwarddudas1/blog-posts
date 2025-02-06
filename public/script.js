6e8cf50f4f15991c695f83c8748528d86fe34dfa
Daniil10Sh <danik2022odessa@gmail.com>
2025-02-06 18:45:19 +0200

    update posts

--- public/script.js
+++ public/script.js
@@ -19,13 +19,35 @@ console.log(jsonData);
 
 
 //   // Оновлення поста
-//   async function updatePost(id, title, content) {
-//     try {
-//            } catch (error) {
-//       console.error(error);
-//     }
-//   }
+let posts = [
+  { id: 1, title: "Перший пост", content: "Це зміст першого поста." },
+  { id: 2, title: "Другий пост", content: "Це зміст другого поста." }
+];
+function updatePost(id, newTitle, newContent) {
+  const post = posts.find(post => post.id === id);
+  if (post) {
+      post.title = newTitle;
+      post.content = newContent;
+      console.log("Пост оновлено:, post");
+  } else {
+      console.log("Пост не знайдено");
+  }
+}
+function editPost(id) {
+  const post = posts.find(post => post.id === id);
+  if (!post) {
+      console.log("Пост не знайдено");
+      return;
+  }
 
+
+  const newTitle = prompt("Введіть новий заголовок:", post.title);
+  const newContent = prompt("Введіть новий зміст:", post.content);
+
+  if (newTitle && newContent) {
+      updatePost(id, newTitle, newContent);
+  }
+}
 
 //   // Видалення поста
 //   async function deletePost(id) {
