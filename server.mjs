67f5abdd7c9d4a239f5ea4074de71c00c6e874bc
olha36 <olhakucheruk36@gmail.com>
2025-02-17 16:26:43 +0200

    delete unnecessary folders, fix server.mjs, add app.delete

--- server.mjs
+++ server.mjs
@@ -3,6 +3,7 @@ import fs from "fs";
 import path from "path";
 import bodyParser from "body-parser";
 import { fileURLToPath } from "url";
+
 const app = express();
 const port = 3000;
 
@@ -29,6 +30,24 @@ app.get("/posts", (req, res) => {
   });
 });
 
+app.get("/posts/:id", (req, res) => {
+  const postId = req.params.id;
+  fs.readFile(path.join(__dirname, "public", "bd.json"), (err, data) => {
+    if (err) {
+      console.error("Error reading database file:", err);
+      res.status(500).send("Error reading database file");
+      return;
+    }
+    const bd = JSON.parse(data);
+    const post = bd.posts.find((p) => p.id === postId);
+    if (!post) {
+      res.status(404).send("Post not found");
+      return;
+    }
+    res.status(200).send(post);
+  });
+});
+
 app.post("/posts", (req, res) => {
   const newPost = req.body;
   fs.readFile(path.join(__dirname, "public", "bd.json"), (error, data) => {
@@ -40,79 +59,131 @@ app.post("/posts", (req, res) => {
     const jsonParseData = JSON.parse(data);
     jsonParseData.posts.push(newPost);
 
-    fs.writeFile(path.join(__dirname, "public", "bd.json"), JSON.stringify(jsonParseData), (error) => {
-      if(error) {
-        console.log(error);
-        res.status(500).send("Error writing file");
-        return;
-      }
-      res.status(201).send('Post is successfully created')
-    })
-  })
-
-})
-
-app.post('/posts/:id/comments', (req, res) => {
-  const newComment = req.body;
-  fs.readFile(path.join(__dirname, "public", "bd.json"), (error, data) => {
-    if (error) {
-      console.log(error);
-      res.status(500).send("Error reading file");
-      return;
-    }
-    const jsonParseData = JSON.parse(data);
-    jsonParseData.comments = jsonParseData.comments || [];
-    jsonParseData.comments.push(newComment);
-
-    fs.writeFile(path.join(__dirname, "public", "bd.json"), JSON.stringify(jsonParseData), (error) => {
-      if (error) {
-        console.log(error);
-        res.status(500).send("Error writing file");
-        return;
-      }
-      res.status(201).send('Comment is successfully created');
-    });
-  });
-});
-
-app.put("/posts/:id", (req, res) => {
-  const getId = req.params.id;
-  const getUpdatedPostData = req.body;
-  fs.readFile(path.join(__dirname, "public", "bd.json"), (error, data) => {
-    if (error) {
-      console.log(error);
-      res.status(500).send("Error reading file");
-      return;
-    }
-    const jsonParseData = JSON.parse(data);
-    const checkIndex = jsonParseData.posts.findIndex(
-      (post) => post.id === getId
-    );
-    if (checkIndex === -1) {
-      res.status(404).send("Post not found");
-      return;
-    }
-
-    jsonParseData.posts[checkIndex] = {
-      ...jsonParseData.posts[checkIndex],
-      ...getUpdatedPostData,
-    };
-
     fs.writeFile(
       path.join(__dirname, "public", "bd.json"),
-      JSON.stringify(jsonParseData),
+      JSON.stringify(jsonParseData, null, 2),
       (error) => {
         if (error) {
           console.log(error);
           res.status(500).send("Error writing file");
           return;
         }
-        res.status(201).send("Post is successfully updated");
+        res.status(201).send("Post is successfully created");
       }
     );
   });
 });
 
+app.put("/posts/:id", (req, res) => {
+  const getId = req.params.id;
+  const getUpdatedPostData = req.body;
+
+  fs.readFile(
+    path.join(__dirname, "public", "bd.json"),
+    "utf8",
+    (error, data) => {
+      if (error) {
+        console.log(error);
+        res.status(500).send("Error reading file");
+        return;
+      }
+      const jsonParseData = JSON.parse(data);
+      const checkIndex = jsonParseData.posts.findIndex(
+        (post) => post.id === getId
+      );
+      if (checkIndex === -1) {
+        res.status(404).send("Post not found");
+        return;
+      }
+
+      jsonParseData.posts[checkIndex] = {
+        ...jsonParseData.posts[checkIndex],
+        ...getUpdatedPostData,
+      };
+
+      fs.writeFile(
+        path.join(__dirname, "public", "bd.json"),
+        JSON.stringify(jsonParseData, null, 2),
+        "utf8",
+        (error) => {
+          if (error) {
+            console.log(error);
+            res.status(500).send("Error writing file");
+            return;
+          }
+          res.status(200).send("Post is successfully updated");
+        }
+      );
+    }
+  );
+});
+
+app.delete("/posts/:id", (req, res) => {
+  fs.readFile(
+    path.join(__dirname, "public", "db.json"),
+    "utf8",
+    (err, data) => {
+      if (err) {
+        console.error("Error reading file:", err);
+        res.status(500).send("Error reading file");
+        return;
+      }
+      const db = JSON.parse(data);
+      db.posts = db.posts.filter((post) => post.id != req.params.id);
+      fs.writeFile(
+        path.join(__dirname, "public", "db.json"),
+        JSON.stringify(db, null, 2),
+        (err) => {
+          if (err) {
+            console.error("Error writing file:", err);
+            res.status(500).send("Error writing file");
+            return;
+          }
+          res.status(200).send("Post deleted");
+        }
+      );
+    }
+  );
+});
+
+app.post("/posts/:id/comments", (req, res) => {
+  const postId = req.params.id;
+  const newComment = req.body.content;
+
+  fs.readFile(
+    path.join(__dirname, "public", "bd.json"),
+    "utf8",
+    (error, data) => {
+      if (error) {
+        console.log(error);
+        res.status(500).send("Error reading file");
+        return;
+      }
+      const jsonParseData = JSON.parse(data);
+      const post = jsonParseData.posts.find((p) => p.id === postId);
+
+      if (!post) {
+        res.status(404).send({ error: "Post not found" });
+        return;
+      }
+      post.comments.push(newComment);
+
+      fs.writeFile(
+        path.join(__dirname, "public", "bd.json"),
+        JSON.stringify(jsonParseData, null, 2),
+        "utf8",
+        (error) => {
+          if (error) {
+            console.log(error);
+            res.status(500).send("Error writing file");
+            return;
+          }
+          res.status(201).send("Comment is successfully created");
+        }
+      );
+    }
+  );
+});
 
 app.listen(port, () => {
   console.log(`Server running on port http://localhost:${port}`);
