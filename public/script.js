4614bf89f67b3bc576510a47e644dd3142118b2b
olha36 <olhakucheruk36@gmail.com>
2025-02-18 21:05:48 +0200

    add stas script.js part

--- public/script.js
+++ public/script.js
@@ -142,6 +142,34 @@ document.addEventListener("click", (event) => {
 
 // Обробник події для додавання коментаря
 //   document.addEventListener('submit', cb);
+document.addEventListener("DOMContentLoaded", function() {
+  let commentsList = document.getElementById("commentsList");
+  let storedComments = JSON.parse(localStorage.getItem("comments")) || [];
+  
+  storedComments.forEach(commentText => {
+      let commentItem = document.createElement("li");
+      commentItem.textContent = commentText;
+      commentsList.appendChild(commentItem);
+  });
+});
+
+document.querySelector(".createCommentForm").addEventListener("submit", function(event) {
+  event.preventDefault();
+  let input = document.querySelector(".commentInput");
+  let commentText = input.value.trim();
+  
+  if (commentText !== "") {
+      let commentItem = document.createElement("li");
+      commentItem.textContent = commentText;
+      document.getElementById("commentsList").appendChild(commentItem);
+      
+      let storedComments = JSON.parse(localStorage.getItem("comments")) || [];
+      storedComments.push(commentText);
+      localStorage.setItem("comments", JSON.stringify(storedComments));
+      
+      input.value = "";
+  }
+});
 
 // Запуск додатку
 async function startApp() {
